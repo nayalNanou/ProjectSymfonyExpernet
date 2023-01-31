79247658af1de12e16c91e8059241f3bb4283fd4
nayalNanou <nayal.christophcolomb@gmail.com>
2023-01-31 09:46:34 +0300

    Creation of ApiSongController

---
+++ src/Controller/SongApiController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SongApiController extends AbstractController
+{
+    #[Route('/api/songs/{id<^[0-9]+$>}', name: 'song_api')]
+    public function apiSongs(int $id): Response
+    {
+        $song = [ 
+            'id' => $id, 
+            'name' => 'Waterfalls', 
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3', 
+        ];
+
+        return new JsonResponse($song);
+    }
+}
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
@@ -11,11 +12,21 @@ class VinylController extends AbstractController
     public function homepage(): Response
     {
         return $this->render('homepage.html.twig', [
-            'songs' => $this->getSongs()
+            'songs' => $this->getSongsTitles()
         ]);
     }
 
-    public function getSongs()
+    #[Route('/browse/{slug}', name: 'browse')]
+    public function browse($slug = '')
+    {
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [ 
+            'genre' => $genre 
+        ]); 
+    }
+
+    public function getSongsTitles()
     {
         return [
             "Gangsta's Paradise - Coolio",
